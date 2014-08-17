a4320d4053834451954b0bd3147c6ed804eb7cf7
sebafonte <sebafonte@gmail.com>
2014-08-17 02:12:21 -0300

    * Web interface update

--- web-server.js
+++ web-server.js
@@ -2,29 +2,35 @@
 var
 http = require('http'),
 path = require('path'),
-fs = require('fs');
+fs = require('fs'),
+net = require('net'),
+url = require('url'),
+querystring = require('querystring');
 
+// SYB init
+
+/*
 // DB init
-//var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/test');
-function run_cmd(cmd, args, cb) {
-  var spawn = require('child_process').spawn
-  var child = spawn(cmd, args);
-  var me = this;
-  child.stdout.on('data', function(me, data) {
-    cb(me, data);
-  });
-}
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+*/
 
+// Command execution function
+function run_cmd(cmd, args, cb) {
+	var spawn = require('child_process').spawn
+	var child = spawn(cmd, args);
+	var me = this;
+	child.stdout.on('data', function(me, data) {
+		cb(me, data);
+	});
+}
 
 //helper function handles file verification
 function getFile(filePath,res,page404){
     //does the requested file exist?
     fs.exists(filePath,function(exists){
         //if it does...
-        console.log(filePath);
-		var a = "D:\\nodejs\\explorer-interfacer/app/dir" == filePath;
-		console.log(a);
+        console.log("PATH: " + filePath);
 		
 		if  ("D:\\nodejs\\explorer-interface/app/dir" == filePath)
 		{
@@ -46,57 +52,147 @@ function getFile(filePath,res,page404){
 				  console.log(foo.stdout);
 				  res.end(foo.stdout);
 				}
+				
 				setTimeout(
-				  // wait 0.25 seconds and print the output
 				  log_console,
 				250);
 		}
-		else if(exists){
-            //read the fiule, run the anonymous function
+		else if (exists){
             fs.readFile(filePath,function(err,contents){
                 if(!err){
-                    //if there was no error
-                    //send the contents with the default 200/ok header
                     res.end(contents);
                 } else {
-                    //for our own troubleshooting
                     console.dir(err);
                 };
             });
-        } else {
-            //if the requested file was not found
-            //serve-up our custom 404 page
-            fs.readFile(page404,function(err,contents){
-                //if there was no error
-                if(!err){
-                    //send the contents with a 404/not found header 
+        } 
+		else {
+            fs.readFile(page404, function(err,contents){
+                if (!err) {
                     res.writeHead(404, {'Content-Type': 'text/html'});
                     res.end(contents);
                 } else {
-                    //for our own troubleshooting
                     console.dir(err);
                 };
             });
         };
     });
+}
+
+function createRandom(res, language, maxSize) {
+	var socket = net.createConnection("20000", "127.0.0.1");
+	var returnData = "default";
+	
+	console.log('Socket created.');
+	
+	socket
+	.on('data', function(data) {
+	  returnData = data;
+	})
+	.on('connect', function() {
+	  socket.write("(make-instance 'tcp-message :name (quote message-web-interface-create-random) :content (list (quote " + language + ") " + maxSize.toString() + "))\n");
+	}).on('end', function() {
+	  console.log('DONE');
+	}).on('close', function(data) {
+	  console.log('CLOSED: ' + socket.remoteAddress +' '+ socket.remotePort);
+    });
+					
+	function log_console() {
+	  console.log("D");
+	  console.log("RETURN DATA 1: " + returnData);
+	  socket.destroy();
+	  res.end(returnData);
+	}
+	
+	console.log("T");
+	setTimeout(log_console,	2000);
+}
+
+function possibleLanguages(res) {
+	var socket = net.createConnection("20000", "127.0.0.1");
+	var returnData = "default";
+	
+	socket
+	.on('data', function(data) {
+	  returnData = data;
+	})
+	.on('connect', function() {
+	  socket.write("(make-instance 'tcp-message :name (quote message-web-interface-get-languages))\n");
+	}).on('end', function() {
+	  console.log('DONE');
+	}).on('close', function(data) {
+	  console.log('CLOSED: ' + socket.remoteAddress +' '+ socket.remotePort);
+    });
+					
+	function log_console() {
+	  socket.destroy();
+	  res.end(returnData);
+	}
+	
+	setTimeout(log_console,	2000);
+}
+
+function mutateImage(res, language, object, maxSize) {
+	var socket = net.createConnection("20000", "127.0.0.1");
+	var data = "";
+	
+	console.log('Socket created.');
+	
+	socket.on('data', function(data) {
+	  console.log('RESPONSE: ' + data);
+	}).on('connect', function() {
+	  socket.write("(make-instance 'tcp-message :name (quote message-web-interface-mutate) :content (list (quote " + language + ") " + object + maxSize.toString() + "))\n");
+	}).on('end', function() {
+	  console.log('DONE');
+	}).on('close', function(data) {
+	  console.log('CLOSED: ' + socket.remoteAddress +' '+ socket.remotePort);
+    });
+	
+	function log_console() {
+	  console.log(foo.stdout);
+	  res.end(foo.stdout);
+	}
+				
+	console.log("T");
+	setTimeout(log_console,	250);
+	socket.destroy();
+	res.write(data);
+	console.log("exiting..");
 };
- 
+
 //a helper function to handle HTTP requests
 function requestHandler(req, res) {
-    var
-    fileName = path.basename(req.url) || 'index.html',
-    localFolder = __dirname + '/app/',
-    page404 = localFolder + '404.html';
- 
-    //call our helper function
-    //pass in the path to the file we want,
-    //the response object, and the 404 page path
-    //in case the requestd file is not found
-    getFile((localFolder + fileName),res,page404);
+    console.log("BASE REQUEST: " + req.url);
+	console.log("BASE REQUEST HEADERS: " + req.headers);
+	
+	if (url.parse(req.url).pathname == "/messageLanguages")	
+	{
+		var arguments = querystring.parse(url.parse(req.url).query);
+		possibleLanguages(res);
+	}
+	else if (url.parse(req.url).pathname == "/messageCreateRandom")	
+	{
+		var arguments = querystring.parse(url.parse(req.url).query);
+		createRandom(res, arguments.language, arguments.maxSize);
+	}	
+	else if (url.parse(req.url).pathname == "/messageMutate")	
+	{
+		var arguments = querystring.parse(url.parse(req.url).query);
+		createImage(res, arguments.language, arguments.object, arguments.maxSize);
+	}		
+	else if (url.parse(req.url).pathname == "/messageCommand")		
+	{
+		var arguments = querystring.parse(url.parse(req.url).query);
+		createImage(res, arguments.language, arguments.command);
+	}
+	else
+	{
+		var fileName = path.basename(req.url) || 'index.html',
+			localFolder = __dirname + '/app/',
+			page404 = localFolder + '404.html';
+		getFile((localFolder + fileName), res, page404);
+	}
 };
- 
-//step 2) create the server
+
 http.createServer(requestHandler)
- 
-//step 3) listen for an HTTP request on port 3000
 .listen(3000);

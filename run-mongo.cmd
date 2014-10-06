1e539c2c85d268bdf14cb16569eac15d93e040d9
sebafonte <sebafonte@gmail.com>
2014-10-06 02:34:04 -0300

    + image vector search pane

---
+++ run-mongo.cmd
@@ -0,0 +1,7 @@
+d:
+cd \
+cd explorer-web
+cd explorer-interface
+cd app
+"c:\Program Files\MongoDB 2.6 Standard\bin\mongod.exe" --repair --dbpath d:\nodejs\explorer-interface\app
+"c:\Program Files\MongoDB 2.6 Standard\bin\mongod.exe" --dbpath d:\nodejs\explorer-interface\app
